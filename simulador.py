5802b9879e3ff837a6b2c70aa128014fe0d367a3
GabrielEsteves-sudo <54445532+GabrielEsteves-sudo@users.noreply.github.com>
2019-12-27 00:35:49 +0000

    Update simulador.py
    FINAL

--- simulador.py
+++ simulador.py
@@ -10,6 +10,7 @@ def encontrar_tempo_lst(lst,elem):
 
 file_to_read = 'tabela_utentes.csv'
 file_to_write= 'sistema.csv'
+file_to_stat= 'estatisticas.txt'
 
 with open(file_to_read) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -33,7 +34,7 @@ with open(file_to_read) as csv_file:
 print(f' -> {line_count} Utentes passarão pelo Sistema')
 #print(lista_utentes)
 with open(file_to_write, mode='w') as tabela_sistema:
-	fieldnames = ['CLOCK', 'TIPO EVENTO', 'UTENTE', 'PROX_CHEGADA', 'LISTA_F1', 'ESTADO_F1', 'PARTIDA_F1', 'LISTA_F2A', 'ESTADO_F2A1', 'PARTIDA_F2A1', 'ESTADO_F2A2', 'PARTIDA_F2A2', 'LISTA_F2B', 'ESTADO_F2B1', 'PARTIDA_F2B1', 'ESTADO_F2B2', 'PARTIDA_F2B2', 'LISTA_F2C', 'ESTADO_F2C', 'PARTIDA_F2C', 'LISTA_F3', 'ESTADO_F3', 'PARTIDA_F3']
+	fieldnames = ['CLOCK', 'TIPO EVENTO', 'UTENTE', 'PROX_CHEGADA', 'LISTA_F1', 'ESTADO_F1', 'PARTIDA_F1', 'LISTA_F2A', 'ESTADO_F2A1', 'PARTIDA_F2A1', 'ESTADO_F2A2', 'PARTIDA_F2A2', 'LISTA_F2B', 'ESTADO_F2B1', 'PARTIDA_F2B1', 'ESTADO_F2B2', 'PARTIDA_F2B2', 'LISTA_F2C', 'ESTADO_F2C', 'PARTIDA_F2C', 'LISTA_F3', 'ESTADO_F3', 'PARTIDA_F3','ESPERA_TOTAL']
 	sistema_writer = csv.DictWriter(tabela_sistema , fieldnames=fieldnames)
 
 	sistema_writer.writeheader()
@@ -82,16 +83,82 @@ with open(file_to_write, mode='w') as tabela_sistema:
 	PARTIDA_F3="-"
 	LISTA_F3=[]
 
+										#ESTATISTICAS
 
+	ESPERA_TOTAL=0
+
+	T1_TAXA_OCUP_A1=0
+	T1_TAXA_OCUP_A2=0
+	T1_TAXA_OCUP_B1=0
+	T1_TAXA_OCUP_B2=0
+	T1_TAXA_OCUP_C=0
+
+	T2_TAXA_OCUP_A1=0
+	T2_TAXA_OCUP_A2=0
+	T2_TAXA_OCUP_B1=0
+	T2_TAXA_OCUP_B2=0
+	T2_TAXA_OCUP_C=0
+
+	T3_TAXA_OCUP_A1=0
+	T3_TAXA_OCUP_A2=0
+	T3_TAXA_OCUP_B1=0
+	T3_TAXA_OCUP_B2=0
+	T3_TAXA_OCUP_C=0
+
+	T4_TAXA_OCUP_A1=0
+	T4_TAXA_OCUP_A2=0
+	T4_TAXA_OCUP_B1=0
+	T4_TAXA_OCUP_B2=0
+	T4_TAXA_OCUP_C=0
+
+	TEMPO_ESPERA=0
+	T1_TEMPO_ESPERA_MAX=0
+	T1_TEMPO_ESPERA_MIN=999999
+	T1_TEMPO_ESPERA_MED=0
+
+	T2_TEMPO_ESPERA_MAX=0
+	T2_TEMPO_ESPERA_MIN=999999
+	T2_TEMPO_ESPERA_MED=0
+
+	T3_TEMPO_ESPERA_MAX=0
+	T3_TEMPO_ESPERA_MIN=999999
+	T3_TEMPO_ESPERA_MED=0
+
+	T4_TEMPO_ESPERA_MAX=0
+	T4_TEMPO_ESPERA_MIN=999999
+	T4_TEMPO_ESPERA_MED=0
+
+	TT_TEMPO_ESPERA_MAX=0
+	TT_TEMPO_ESPERA_MIN=999999
+	TT_TEMPO_ESPERA_MED=0
+
+	T1_CONT_ESPERA=0
+	T2_CONT_ESPERA=0
+	T3_CONT_ESPERA=0
+	T4_CONT_ESPERA=0
+
+	T1_TEMPO_ESPERA=0
+	T2_TEMPO_ESPERA=0
+	T3_TEMPO_ESPERA=0
+	T4_TEMPO_ESPERA=0
+	
+	CONT_ESPERA=0
+
+
+	LST_TEMPOS_ESPERA=[]
 
 	
 	print("ciclo")
 	
 
 	while (C < tempo_final) or (len(LISTA_EVENTO) != 0):          #até o clock acabar ou até que não haja mais cientes para atender
+		TEMPO_ESPERA=0
+
 		if encontrar_tempo_lst(LISTA_EVENTO,C):
 			LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
-			
+
+			TEMPO_ESPERA=0
+
 			EVENTO = LISTA_EVENTO[0]
 			UTENTE = EVENTO[2]
 
@@ -112,6 +179,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F3) != 0):
 					EVENTO = LISTA_F3.pop(0)
 					ESTADO_F3="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F3 = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][6])
 					LISTA_EVENTO.append((int(PARTIDA_F3),"PF3",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -181,6 +253,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F2A) != 0):
 					EVENTO = LISTA_F2A.pop(0)
 					ESTADO_F2A1="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F2A1 = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][3])
 					LISTA_EVENTO.append((int(PARTIDA_F2A1),"PF2A1",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -205,6 +282,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F2A) != 0):
 					EVENTO = LISTA_F2A.pop(0)
 					ESTADO_F2A2="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F2A2 = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][3])
 					LISTA_EVENTO.append((int(PARTIDA_F2A2),"PF2A2",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -228,6 +310,10 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F2B) != 0):
 					EVENTO = LISTA_F2B.pop(0)
 					ESTADO_F2B1="OCUPADO"
+					ESPERA_TOTAL=ESPERA_TOTAL+(C-int(EVENTO[0]))
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F2B1 = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][3])
 					LISTA_EVENTO.append((int(PARTIDA_F2B1),"PF2B1",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -251,6 +337,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F2B) != 0):
 					EVENTO = LISTA_F2B.pop(0)
 					ESTADO_F2B2="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F2B2 = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][3])
 					LISTA_EVENTO.append((int(PARTIDA_F2B2),"PF2B2",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -275,6 +366,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 				if(len(LISTA_F2C) != 0):
 					EVENTO = LISTA_F2C.pop(0)
 					ESTADO_F2C="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					PARTIDA_F2C = C+ int (lista_utentes[int(EVENTO[2].split(" ")[1])-1][3])
 					LISTA_EVENTO.append((int(PARTIDA_F2C),"PF2C",EVENTO[2]))
 					LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
@@ -341,6 +437,11 @@ with open(file_to_write, mode='w') as tabela_sistema:
 					LISTA_F1 = sorted(LISTA_F1, key=lambda element: (element[2], element[0]))
 					EVENTO = LISTA_F1.pop(0)
 					ESTADO_F1="OCUPADO"
+					TEMPO_ESPERA = (C-int(EVENTO[0]))
+					ESPERA_TOTAL=ESPERA_TOTAL+TEMPO_ESPERA
+					if(TEMPO_ESPERA != 0) :
+						LST_TEMPOS_ESPERA.append(TEMPO_ESPERA)
+						CONT_ESPERA += 1
 					#print(f'EVENTO[2]-{EVENTO[2]}')
 					#print(f'EVENTO[2].split(" ")-{EVENTO[2].split(" ")}')
 					#print(f'EVENTO[2].split(" ")[1]-{EVENTO[2].split(" ")[1]}')
@@ -362,15 +463,18 @@ with open(file_to_write, mode='w') as tabela_sistema:
 					LISTA_F1.append((int(C),"PF1",UTENTE)) #lista_utentes[INDEX_PROX_UTENTE-1][0]
 					LISTA_F1 = sorted(LISTA_F1, key=lambda element: (element[2], element[0]))
 			else:
-				print("CORREU MAL ALGUMA COISA")
+				print("VETOR DE EVENTO FOI INICIALIZADO")
 
 				
 
 
-			sistema_writer.writerow({'CLOCK' : C, 'TIPO EVENTO' : EVENTO[1] ,'UTENTE' : UTENTE, 'PROX_CHEGADA' : PROX_CHEGADA, 'LISTA_F1' : LISTA_F1, 'ESTADO_F1' : ESTADO_F1, 'PARTIDA_F1' : PARTIDA_F1, 'LISTA_F2A' : LISTA_F2A, 'ESTADO_F2A1' : ESTADO_F2A1, 'PARTIDA_F2A1' : PARTIDA_F2A1, 'ESTADO_F2A2' : ESTADO_F2A2, 'PARTIDA_F2A2' : PARTIDA_F2A2, 'LISTA_F2B' : LISTA_F2B, 'ESTADO_F2B1' : ESTADO_F2B1, 'PARTIDA_F2B1' : PARTIDA_F2B1, 'ESTADO_F2B2' : ESTADO_F2B2, 'PARTIDA_F2B2' : PARTIDA_F2B2, 'LISTA_F2C' : LISTA_F2C, 'ESTADO_F2C' : ESTADO_F2C, 'PARTIDA_F2C' : PARTIDA_F2C, 'LISTA_F3' : LISTA_F3, 'ESTADO_F3' : ESTADO_F3, 'PARTIDA_F3' : PARTIDA_F3})
+			sistema_writer.writerow({'CLOCK' : C, 'TIPO EVENTO' : EVENTO[1] ,'UTENTE' : UTENTE, 'PROX_CHEGADA' : PROX_CHEGADA, 'LISTA_F1' : LISTA_F1, 'ESTADO_F1' : ESTADO_F1, 'PARTIDA_F1' : PARTIDA_F1, 'LISTA_F2A' : LISTA_F2A, 'ESTADO_F2A1' : ESTADO_F2A1, 'PARTIDA_F2A1' : PARTIDA_F2A1, 'ESTADO_F2A2' : ESTADO_F2A2, 'PARTIDA_F2A2' : PARTIDA_F2A2, 'LISTA_F2B' : LISTA_F2B, 'ESTADO_F2B1' : ESTADO_F2B1, 'PARTIDA_F2B1' : PARTIDA_F2B1, 'ESTADO_F2B2' : ESTADO_F2B2, 'PARTIDA_F2B2' : PARTIDA_F2B2, 'LISTA_F2C' : LISTA_F2C, 'ESTADO_F2C' : ESTADO_F2C, 'PARTIDA_F2C' : PARTIDA_F2C, 'LISTA_F3' : LISTA_F3, 'ESTADO_F3' : ESTADO_F3, 'PARTIDA_F3' : PARTIDA_F3, 'ESPERA_TOTAL' : ESPERA_TOTAL})
 			LISTA_EVENTO = sorted(LISTA_EVENTO, key=lambda element: (element[0], element[2]))
 			print(f'\nCLOCK- {C}')
 			print(f'1-INSERE- {LISTA_EVENTO}')
+
+			
+
 
 			LISTA_EVENTO.pop(0)
 			print(f'2-REMOVE- {LISTA_EVENTO}')
@@ -390,5 +494,211 @@ with open(file_to_write, mode='w') as tabela_sistema:
 			
 		else:
 			C += 1
+			if (C <= 60*60*2 ):
+				if (ESTADO_F2A1 == "OCUPADO"):
+					T1_TAXA_OCUP_A1 += 1
+				if (ESTADO_F2A2 == "OCUPADO"):
+					T1_TAXA_OCUP_A2 += 1
+				if (ESTADO_F2B1 == "OCUPADO"):
+					T1_TAXA_OCUP_B1 += 1
+				if (ESTADO_F2B2 == "OCUPADO"):
+					T1_TAXA_OCUP_B2 += 1
+				if (ESTADO_F2C == "OCUPADO"):
+					T1_TAXA_OCUP_C += 1
+
+				T1_CONT_ESPERA = T1_CONT_ESPERA + CONT_ESPERA
+				for i in LST_TEMPOS_ESPERA:
+					if(T1_TEMPO_ESPERA_MAX < i):
+						T1_TEMPO_ESPERA_MAX=i
+
+					if(i != 0 and T1_TEMPO_ESPERA_MIN > i):
+						T1_TEMPO_ESPERA_MIN=i
+					T1_TEMPO_ESPERA=T1_TEMPO_ESPERA + i
+						
+
+
+					
+
+			elif (C <= 60*60*2*2):
+				if (ESTADO_F2A1 == "OCUPADO"):
+					T2_TAXA_OCUP_A1 += 1
+				if (ESTADO_F2A2 == "OCUPADO"):
+					T2_TAXA_OCUP_A2 += 1
+				if (ESTADO_F2B1 == "OCUPADO"):
+					T2_TAXA_OCUP_B1 += 1
+				if (ESTADO_F2B2 == "OCUPADO"):
+					T2_TAXA_OCUP_B2 += 1
+				if (ESTADO_F2C == "OCUPADO"):
+					T2_TAXA_OCUP_C += 1
+
+				T2_CONT_ESPERA = T2_CONT_ESPERA + CONT_ESPERA
+				for i in LST_TEMPOS_ESPERA:
+					if(T2_TEMPO_ESPERA_MAX < i):
+						T2_TEMPO_ESPERA_MAX=i
+
+					if(i != 0 and T2_TEMPO_ESPERA_MIN > i):
+						T2_TEMPO_ESPERA_MIN=i
+					T2_TEMPO_ESPERA=T2_TEMPO_ESPERA + i
+
+			elif (C <= 60*60*2*3):
+				if (ESTADO_F2A1 == "OCUPADO"):
+					T3_TAXA_OCUP_A1 += 1
+				if (ESTADO_F2A2 == "OCUPADO"):
+					T3_TAXA_OCUP_A2 += 1
+				if (ESTADO_F2B1 == "OCUPADO"):
+					T3_TAXA_OCUP_B1 += 1
+				if (ESTADO_F2B2 == "OCUPADO"):
+					T3_TAXA_OCUP_B2 += 1
+				if (ESTADO_F2C == "OCUPADO"):
+					T3_TAXA_OCUP_C += 1
+
+				T3_CONT_ESPERA = T3_CONT_ESPERA + CONT_ESPERA
+				for i in LST_TEMPOS_ESPERA:
+					if(T3_TEMPO_ESPERA_MAX < i):
+						T3_TEMPO_ESPERA_MAX=i
+
+					if(i != 0 and T3_TEMPO_ESPERA_MIN > i):
+						T3_TEMPO_ESPERA_MIN=i
+					T3_TEMPO_ESPERA=T3_TEMPO_ESPERA + i
+
+			else:
+				if (ESTADO_F2A1 == "OCUPADO"):
+					T4_TAXA_OCUP_A1 += 1
+				if (ESTADO_F2A2 == "OCUPADO"):
+					T4_TAXA_OCUP_A2 += 1
+				if (ESTADO_F2B1 == "OCUPADO"):
+					T4_TAXA_OCUP_B1 += 1
+				if (ESTADO_F2B2 == "OCUPADO"):
+					T4_TAXA_OCUP_B2 += 1
+				if (ESTADO_F2C == "OCUPADO"):
+					T4_TAXA_OCUP_C += 1
+
+				T4_CONT_ESPERA = T4_CONT_ESPERA + CONT_ESPERA
+				for i in LST_TEMPOS_ESPERA:
+					if(T4_TEMPO_ESPERA_MAX < i):
+						T4_TEMPO_ESPERA_MAX=i
+
+					if(i != 0 and T4_TEMPO_ESPERA_MIN > i):
+						T4_TEMPO_ESPERA_MIN=i
+					T4_TEMPO_ESPERA=T4_TEMPO_ESPERA + i
+
+			CONT_ESPERA=0
+			LST_TEMPOS_ESPERA=[]
 		#print(C)
-print(f'foi criado um ficheiro -> \"{file_to_write}\"\n com a INFORMACAO DE CADA PASSO DADO no sistema \nApartir dos dados gerados , que são lidos \ndo ficheiro -> \"{file_to_read}\"')
+	
+print(f'Foi criado um ficheiro -> \"{file_to_write}\"\n com a INFORMACAO DE CADA PASSO DADO no sistema \n')
+print(f'E foi criado um ficheiro -> \"{file_to_stat}\"\n com as ESTATISTICAS OBTIDAS do sistema \n')
+print(f'Apartir dos dados gerados , que são lidos \ndo ficheiro -> \"{file_to_read}\"')
+#with open(file_to_stat, mode='a') as tabela_estatisticas:
+	#fieldnames = ['T1_TAXA_OCUP_A1','T1_TAXA_OCUP_A2','T1_TAXA_OCUP_B1','T1_TAXA_OCUP_B2','T1_TAXA_OCUP_C','T2_TAXA_OCUP_A1','T2_TAXA_OCUP_A2','T2_TAXA_OCUP_B1','T2_TAXA_OCUP_B2','T2_TAXA_OCUP_C','T3_TAXA_OCUP_A1','T3_TAXA_OCUP_A2','T3_TAXA_OCUP_B1','T3_TAXA_OCUP_B2','T3_TAXA_OCUP_C','T4_TAXA_OCUP_A1','T4_TAXA_OCUP_A2','T4_TAXA_OCUP_B1','T4_TAXA_OCUP_B2','T4_TAXA_OCUP_C','TOTAL_TAXA_OCUP_A1','TOTAL_TAXA_OCUP_A2','TOTAL_TAXA_OCUP_B1','TOTAL_TAXA_OCUP_B2','TOTAL_TAXA_OCUP_C']]
+	#estatisticas_writer = csv.DictWriter(tabela_estatisticas , fieldnames=fieldnames)
+	#estatisticas_writer.writeheader()
+file_estatisticas=open(file_to_stat, mode='w')
+
+#TEMPOS DE ESPERA
+
+TT_CONT_ESPERA = T1_CONT_ESPERA + T2_CONT_ESPERA + T3_CONT_ESPERA + T4_CONT_ESPERA
+TT_TEMPO_ESPERA=  T1_TEMPO_ESPERA + T2_TEMPO_ESPERA + T3_TEMPO_ESPERA + T4_TEMPO_ESPERA
+
+TT_TEMPO_ESPERA_MIN = min(T1_TEMPO_ESPERA_MIN,T2_TEMPO_ESPERA_MIN,T3_TEMPO_ESPERA_MIN,T4_TEMPO_ESPERA_MIN)
+if(TT_TEMPO_ESPERA_MIN == 999999):
+	TT_TEMPO_ESPERA_MIN=0
+
+if(T1_TEMPO_ESPERA_MIN == 999999):
+	T1_TEMPO_ESPERA_MIN=0
+if(T2_TEMPO_ESPERA_MIN == 999999):
+	T2_TEMPO_ESPERA_MIN=0
+if(T3_TEMPO_ESPERA_MIN == 999999):
+	T3_TEMPO_ESPERA_MIN=0
+if(T4_TEMPO_ESPERA_MIN == 999999):
+	T4_TEMPO_ESPERA_MIN=0
+
+TT_TEMPO_ESPERA_MAX = max(T1_TEMPO_ESPERA_MAX,T2_TEMPO_ESPERA_MAX,T3_TEMPO_ESPERA_MAX,T4_TEMPO_ESPERA_MAX)
+if(TT_TEMPO_ESPERA_MAX == 0):
+	TT_TEMPO_ESPERA_MAX=0
+
+if(T1_TEMPO_ESPERA_MAX == 0):
+	T1_TEMPO_ESPERA_MAX=0
+if(T2_TEMPO_ESPERA_MAX == 0):
+	T2_TEMPO_ESPERA_MAX=0
+if(T3_TEMPO_ESPERA_MAX == 0):
+	T3_TEMPO_ESPERA_MAX=0
+if(T4_TEMPO_ESPERA_MAX == 0):
+	T4_TEMPO_ESPERA_MAX=0
+
+if(T1_CONT_ESPERA != 0):
+	T1_TEMPO_ESPERA_MED = T1_TEMPO_ESPERA/T1_CONT_ESPERA
+else:
+	T1_TEMPO_ESPERA_MED = 0
+if(T2_CONT_ESPERA != 0):
+	T2_TEMPO_ESPERA_MED = T2_TEMPO_ESPERA/T2_CONT_ESPERA
+else:
+	T2_TEMPO_ESPERA_MED = 0
+if(T3_CONT_ESPERA != 0):
+	T3_TEMPO_ESPERA_MED = T3_TEMPO_ESPERA/T3_CONT_ESPERA
+else:
+	T3_TEMPO_ESPERA_MED = 0
+if(T4_CONT_ESPERA != 0):
+	T4_TEMPO_ESPERA_MED = T4_TEMPO_ESPERA/T4_CONT_ESPERA
+else:
+	T4_TEMPO_ESPERA_MED = 0
+if(TT_CONT_ESPERA != 0):
+	TT_TEMPO_ESPERA_MED = TT_TEMPO_ESPERA/TT_CONT_ESPERA
+else:
+	TT_TEMPO_ESPERA_MED = 0
+
+
+#TAXAS DE OCUPACAO
+
+T1_A1 = (T1_TAXA_OCUP_A1*100)/(60*60*2)
+T1_A2 = (T1_TAXA_OCUP_A2*100)/(60*60*2)
+T1_B1 = (T1_TAXA_OCUP_B1*100)/(60*60*2)
+T1_B2 = (T1_TAXA_OCUP_B2*100)/(60*60*2)
+T1_C =  (T1_TAXA_OCUP_C*100)/(60*60*2)
+
+T2_A1 = (T2_TAXA_OCUP_A1*100)/(60*60*2)
+T2_A2 = (T2_TAXA_OCUP_A2*100)/(60*60*2)
+T2_B1 = (T2_TAXA_OCUP_B1*100)/(60*60*2)
+T2_B2 = (T2_TAXA_OCUP_B2*100)/(60*60*2)
+T2_C =  (T2_TAXA_OCUP_C*100)/(60*60*2)
+
+T3_A1 = (T3_TAXA_OCUP_A1*100)/(60*60*2)
+T3_A2 = (T3_TAXA_OCUP_A2*100)/(60*60*2)
+T3_B1 = (T3_TAXA_OCUP_B1*100)/(60*60*2)
+T3_B2 = (T3_TAXA_OCUP_B2*100)/(60*60*2)
+T3_C =  (T3_TAXA_OCUP_C*100)/(60*60*2)
+
+T4_A1 = (T4_TAXA_OCUP_A1*100)/(60*60*2)
+T4_A2 = (T4_TAXA_OCUP_A2*100)/(60*60*2)
+T4_B1 = (T4_TAXA_OCUP_B1*100)/(60*60*2)
+T4_B2 = (T4_TAXA_OCUP_B2*100)/(60*60*2)
+T4_C =  (T4_TAXA_OCUP_C*100)/(60*60*2)
+
+TT_A1 = ((T1_TAXA_OCUP_A1+T2_TAXA_OCUP_A1+T3_TAXA_OCUP_A1+T4_TAXA_OCUP_A1)*100)/(tempo_final)
+TT_A2 = ((T1_TAXA_OCUP_A2+T2_TAXA_OCUP_A2+T3_TAXA_OCUP_A2+T4_TAXA_OCUP_A2)*100)/(tempo_final)
+TT_B1 = ((T1_TAXA_OCUP_B1+T2_TAXA_OCUP_B1+T3_TAXA_OCUP_B1+T4_TAXA_OCUP_B1)*100)/(tempo_final)
+TT_B2 = ((T1_TAXA_OCUP_B2+T2_TAXA_OCUP_B2+T3_TAXA_OCUP_B2+T4_TAXA_OCUP_B2)*100)/(tempo_final)
+TT_C =  ((T1_TAXA_OCUP_C +T2_TAXA_OCUP_C +T3_TAXA_OCUP_C +T4_TAXA_OCUP_C )*100)/(tempo_final)
+
+
+file_estatisticas.write(f'TEmed = Tempo de Espera Médio\nTEmax = Tempo de Espera Máximo\nTEmin = Tempo de Espera Minimo\nTEtot = Tempo de Espera Total\nUtot = Utentes que tiveram de Esperar\n')
+file_estatisticas.write(f'TdO = Taxa de Ocupacao\nT1 = 9h até às 11h\nT2 = 11h até às 13h\nT3 = 13h até às 15h\nT4 = 15h até às 17h\nTT =Tempo Total\n')
+file_estatisticas.write(f'A1 = Balcao 1 com o Assunto A\nA2 = Balcao 2 com o Assunto A\nB1 = Balcao 1 com o Assunto B\nB2 = Balcao 2 com o Assunto B\nC = Único Balcao com o Assunto C\n\n')
+
+file_estatisticas.write(f'TEmed A1 T1 = {("%.3f" % T1_TEMPO_ESPERA_MED)}seg    \tTEmax A2 T1 = {( T1_TEMPO_ESPERA_MAX)}seg    \tTEmin B1 T1 = {( T1_TEMPO_ESPERA_MIN)}seg    \tTEtot B2 T1 = {( T1_TEMPO_ESPERA)}seg   \t Utot C T1 = {(T1_CONT_ESPERA)} utentes\n')
+file_estatisticas.write(f'TEmed A1 T2 = {("%.3f" % T2_TEMPO_ESPERA_MED)}seg    \tTEmax A2 T2 = {( T2_TEMPO_ESPERA_MAX)}seg    \tTEmin B1 T2 = {( T2_TEMPO_ESPERA_MIN)}seg    \tTEtot B2 T2 = {( T2_TEMPO_ESPERA)}seg   \t Utot C T2 = {(T2_CONT_ESPERA)} utentes\n')
+file_estatisticas.write(f'TEmed A1 T3 = {("%.3f" % T3_TEMPO_ESPERA_MED)}seg    \tTEmax A2 T3 = {( T3_TEMPO_ESPERA_MAX)}seg    \tTEmin B1 T3 = {( T3_TEMPO_ESPERA_MIN)}seg    \tTEtot B2 T3 = {( T3_TEMPO_ESPERA)}seg   \t Utot C T3 = {(T3_CONT_ESPERA)} utentes\n')
+file_estatisticas.write(f'TEmed A1 T4 = {("%.3f" % T4_TEMPO_ESPERA_MED)}seg    \tTEmax A2 T4 = {( T4_TEMPO_ESPERA_MAX)}seg    \tTEmin B1 T4 = {( T4_TEMPO_ESPERA_MIN)}seg    \tTEtot B2 T4 = {( T4_TEMPO_ESPERA)}seg   \t Utot C T4 = {(T4_CONT_ESPERA)} utentes\n')
+file_estatisticas.write(f'TEmed A1 TT = {("%.3f" % TT_TEMPO_ESPERA_MED)}seg    \tTEmax A2 TT = {( TT_TEMPO_ESPERA_MAX)}seg    \tTEmin B1 TT = {( TT_TEMPO_ESPERA_MIN)}seg    \tTEtot B2 TT = {( TT_TEMPO_ESPERA)}seg   \t Utot C TT = {(TT_CONT_ESPERA)} utentes\n')
+
+file_estatisticas.write(f'\n')
+
+file_estatisticas.write(f'TdO A1 T1 = {("%.3f" % T1_A1)}%   \tTdO A2 T1 = {("%.3f" % T1_A2)}%   \tTdO B1 T1 = {("%.3f" % T1_B1)}%   \tTdO B2 T1 = {("%.3f" % T1_B2)}%   \tTdO C T1 = {("%.3f" % T1_C)}%\n')
+file_estatisticas.write(f'TdO A1 T2 = {("%.3f" % T2_A1)}%   \tTdO A2 T2 = {("%.3f" % T2_A2)}%   \tTdO B1 T2 = {("%.3f" % T2_B1)}%   \tTdO B2 T2 = {("%.3f" % T2_B2)}%   \tTdO C T2 = {("%.3f" % T2_C)}%\n')
+file_estatisticas.write(f'TdO A1 T3 = {("%.3f" % T3_A1)}%   \tTdO A2 T3 = {("%.3f" % T3_A2)}%   \tTdO B1 T3 = {("%.3f" % T3_B1)}%   \tTdO B2 T3 = {("%.3f" % T3_B2)}%   \tTdO C T3 = {("%.3f" % T3_C)}%\n')
+file_estatisticas.write(f'TdO A1 T4 = {("%.3f" % T4_A1)}%   \tTdO A2 T4 = {("%.3f" % T4_A2)}%   \tTdO B1 T4 = {("%.3f" % T4_B1)}%   \tTdO B2 T4 = {("%.3f" % T4_B2)}%   \tTdO C T4 = {("%.3f" % T4_C)}%\n')
+file_estatisticas.write(f'TdO A1 TT = {("%.3f" % TT_A1)}%   \tTdO A2 TT = {("%.3f" % TT_A2)}%   \tTdO B1 TT = {("%.3f" % TT_B1)}%   \tTdO B2 TT = {("%.3f" % TT_B2)}%   \tTdO C TT = {("%.3f" % TT_C)}%\n')
+
+#estatisticas_writer.writerow({})
+
+file_estatisticas.close()
